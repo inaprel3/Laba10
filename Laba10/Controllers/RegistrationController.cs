d2eed070e2e9744d04c7022ef484eb97efe16d52
Nastya <staprost.22@gmail.com>
2024-03-30 16:22:13 +0100

    Добавьте файлы проекта.

---
+++ Laba10/Controllers/RegistrationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Laba10.Models;
+
+namespace Laba10.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        private static List<RegistrationForm> _registrations = new List<RegistrationForm>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegistrationForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                _registrations.Add(model);
+                int lastIndex = _registrations.Count - 1;
+                return RedirectToAction("Confirmation", new { id = lastIndex });
+            }
+
+            return View(model);
+        }
+
+        public IActionResult Confirmation(int id)
+        {
+            var model = _registrations[id];
+            return View(model);
+        }
+    }
+}
---
+++ Laba10/Models/RegistrationForm.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Laba10.Models
+{
+    public class RegistrationForm
+    {
+        [Required(ErrorMessage = "Поле 'Ім'я прізвище' є обов'язковим")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Email' є обов'язковим")]
+        [EmailAddress(ErrorMessage = "Введіть коректний Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Поле 'Бажана дата консультації' є обов'язковим")]
+        [FutureDate(ErrorMessage = "Дата має бути у майбутньому")]
+        public DateTime ConsultationDate { get; set; }
+
+        [Required(ErrorMessage = "Виберіть продукт")]
+        public string SelectedProduct { get; set; }
+    }
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime date;
+            if (DateTime.TryParse(value.ToString(), out date))
+            {
+                return date.Date > DateTime.Now.Date && date.DayOfWeek != DayOfWeek.Saturday && date.DayOfWeek != DayOfWeek.Sunday;
+            }
+            return false;
+        }
+    }
+}
